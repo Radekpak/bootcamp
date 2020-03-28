19bf00b9a203640773dedd85ef147528dd17eecf
radek <radekpak13@gmail.com>
2020-03-28 14:03:05 +0100

    initial

---
+++ new1.py
@@ -0,0 +1,20 @@
+#Pole trójkąta = a*h/2
+a=10
+h=5
+polet= (a*h/2)
+print (polet)
+#Pole koła = pi*r**2
+r=7
+pi=3.14
+polek=(pi*r**2)
+print(polek)
+#Trapez = (a+b) *h/2
+b=3
+c=9
+h=6.5
+polet2=((b+c)*h/2)
+print(polet2)
+#Kula = (4/3)*pi*r**3
+r1=0.875
+polek2=(4/3*pi*r1**3)
+print(polek2)
